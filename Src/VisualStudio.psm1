b9ce7717423d2860ec79d3e90ff845796499acfe
Jonathan Matheus <jmatheus@gmail.com>
2011-04-25 22:24:13 -0500

    Fixed SetStartupProjects to include projects nested within solution folders

--- Src/VisualStudio.psm1
+++ Src/VisualStudio.psm1
@@ -8,6 +8,8 @@ function Set-StartupProjects {
 	
 	write-verbose "Finding startup projects"
 	
+	[System.Reflection.Assembly]::Load("EnvDTE100, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a") | out-null
+	
 	$dte = new-object -com VisualStudio.DTE.10.0 -strict
 	
 	#HACK: Com objects suck in PowerShell
@@ -20,24 +22,33 @@ function Set-StartupProjects {
 	#$sln = $dte.Solution
 	$sln = [System.__ComObject].InvokeMember("Solution",[System.Reflection.BindingFlags]::GetProperty,$null,$dte,$null)
 	
+	write-verbose ("Solution: " + $solutionFile)
+	
 	#$sln.Open($solutionFile)
 	[System.__ComObject].InvokeMember("Open",[System.Reflection.BindingFlags]::InvokeMethod,$null,$sln,$solutionFile)
+
+	#HACK:I don't know why InvokeMember craps out on collections, so I made a dynamic c# class that does the casting
+	$helper = GetHelper
 	
-	#$projects = $sln.Projects | Where-Object { $_.Name -match $includeRegex }
-	$projects = [System.__ComObject].InvokeMember("Projects",[System.Reflection.BindingFlags]::GetProperty,$null,$sln,$null)
+	$projects = $helper.GetProjectsForSolution($sln)
+		
+	write-verbose "Available projects:"
+	$projects | foreach { write-verbose $_ }
 	
-	$projectNames = $projects | foreach { $_.UniqueName }
+	[string[]] $startupProjects = $projects | Where-Object { $_ -match $includeRegex }
 	
-	[string[]] $startupProjects = $projectNames | Where-Object { $_ -match $includeRegex }
+	if($startupProjects -ne $null) {
+		write-host "Found the following startup projects:"
+		$startupProjects | foreach { write-host $_ }
+	}
+	else {
+		write-host "No startup projects were found"
+	}
 	
-	write-host "Found the following startup projects:"
-	$startupProjects | foreach { write-host $_ }
-	
-	#HACK:I don't know why InvokeMember craps out on collections
-	SetStartupProjectsForSolution $sln $startupProjects
+	$helper.SetStartupProjects($sln, $startupProjects) | out-null
 }
 
-function SetStartupProjectsForSolution {
+function GetHelper {
 	param(
 		$solution,
 		[string[]] $startupProjects
@@ -51,24 +62,61 @@ function SetStartupProjectsForSolution {
 	)
 	
 	Add-Type -ReferencedAssemblies $refs @"
-public class SetStartupForSolution {
-	public void Set(object solution, string[] items) {
+public class SolutionHelper {
+	public void SetStartupProjects(object solution, string[] items) {
 		EnvDTE100.Solution4 sln4 = (EnvDTE100.Solution4)solution;
 		
-		object[] items2 = new object[items.Length];
+		var length = items != null ? items.Length : 0;
 		
-		for(int i=0; i<items.Length; i++) {
+		object[] items2 = new object[length];
+		
+		for(int i=0; i < length; i++) {
 			items2[i] = items[i];
 		}
 		
 		sln4.SolutionBuild.StartupProjects = items2;
 	}
+	
+	public System.Collections.Generic.IEnumerable<string> GetProjectsForSolution(object solution) 
+	{
+		var sln4 = (EnvDTE100.Solution4)solution;
+		
+		var projects = new System.Collections.Generic.List<string>();
+		
+		foreach(EnvDTE.Project project in sln4.Projects) {
+			projects.AddRange(GetProjects(project));
+		}
+		
+		return projects;
+	}
+
+	public System.Collections.Generic.IEnumerable<string> GetProjects(EnvDTE.Project project) 
+	{
+		var isAProject = project.ConfigurationManager != null;
+		
+		if(isAProject)
+		{
+			yield return project.UniqueName;
+		}
+		else 
+		{
+			foreach(EnvDTE.ProjectItem item in project.ProjectItems) {
+				if(item.SubProject != null) {
+					var subProjects = GetProjects(item.SubProject);
+					
+					foreach(var subProject in subProjects) {
+						yield return subProject;
+					}
+				}
+			}
+		}
+	}
 }
 "@
 	
-	$setter = new-object SetStartupForSolution
+	$helper = new-object SolutionHelper
 	
-	$setter.Set($solution, $startupProjects) | out-null
+	return $helper
 }
 
 Export-ModuleMember -Function "Set-StartupProjects"
